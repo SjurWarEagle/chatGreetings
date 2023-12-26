a9c7555abb8904ce5f67a7b4e1e54ee57623147f
Torsten Kunkel <git@tkunkel.de>
2023-12-26 04:24:27 +0100

    rename docker image

--- docker/build.cmd
+++ docker/build.cmd
@@ -1,2 +1,2 @@
 REM run in docker-folder
-docker build -f Dockerfile .. --progress=plain --no-cache -t chat-greetings:latest
+docker build -f Dockerfile .. --progress=plain --no-cache -t twitch-greetings:latest
